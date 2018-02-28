55079bed8f200f63d13774c407d1c6f5ed8a5219
vyadzmak <vyadzmak@gmail.com>
2018-02-28 18:49:50 +0300

    remove unpickable analytic elements

--- modules/analytic_rules_parser.py
+++ modules/analytic_rules_parser.py
@@ -6,7 +6,7 @@ def encode(ob):
         jsonpickle.set_preferred_backend('json')
         jsonpickle.set_encoder_options('json', ensure_ascii=False)
         #jsonpickle.set_encoder_options('simplejson', sort_keys=True, indent=4)
-        json_s = jsonpickle.encode(ob, unpicklable=True)
+        json_s = jsonpickle.encode(ob, unpicklable=False)
         return json_s
     except Exception as e:
         print(str(e))
