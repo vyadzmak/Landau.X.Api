49c837370d1171fd4b2157ff763024272e919e60
vyadzmak <vyadzmak@gmail.com>
2018-03-01 13:03:17 +0300

    added CRUD operations for users, clients etc

--- res/clients_resources.py
+++ res/clients_resources.py
@@ -1,4 +1,5 @@
-from db_models.models import Clients
+from db_models.models import AnalyticRules, Clients, Users, UserLogins, Projects, Documents, ProjectControlLog, \
+    ProjectAnalysisLog, Reports, ReportForms, ProjectAnalysis, TransferCellsParams
 from db.db import session
 from flask import Flask, jsonify, request
 from flask_restful import Resource, fields, marshal_with, abort, reqparse
@@ -19,9 +20,6 @@ client_fields = {
 }
 
 
-
-
-
 class ClientResource(Resource):
     @marshal_with(client_fields)
     def get(self, id):
@@ -35,18 +33,67 @@ class ClientResource(Resource):
         if not client:
             abort(404, message="Client type {} doesn't exist".format(id))
 
-        client_id = client.client_type_id
+        client_id = client.id
+        users = session.query(Users).filter(Users.client_id == client_id).all()
+        # userlogins
+        # users
+        for user in users:
+            projects = session.query(Projects).filter(Projects.user_id == user.id).all()
+            for project in projects:
+                docs = session.query(Documents).filter(Documents.project_id == project.id).all()
+                reports = session.query(Reports).filter(Reports.project_id == project.id).all()
+                reportForms = session.query(ReportForms).filter(ReportForms.project_id == project.id).all()
+                logs = session.query(ProjectAnalysisLog).filter(ProjectAnalysisLog.project_id == project.id).all()
+                analysis = session.query(ProjectAnalysis).filter(ProjectAnalysis.project_id == project.id).all()
+                control_logs = session.query(ProjectControlLog).filter(ProjectControlLog.project_id == project.id).all()
+                transfer_cells_params = session.query(TransferCellsParams).filter(
+                    TransferCellsParams.project_id == project.id).all()
 
-        #userlogins
-        #users
+                for doc in docs:
+                    session.delete(doc)
+                    session.commit()
 
+                for report in reports:
+                    session.delete(report)
+                    session.commit()
 
+                for report_form in reportForms:
+                    session.delete(report_form)
+                    session.commit()
 
+                for log in logs:
+                    session.delete(log)
+                    session.commit()
+
+                for analys in analysis:
+                    session.delete(analys)
+                    session.commit()
+
+                for log in control_logs:
+                    session.delete(log)
+                    session.commit()
+
+                for transfer_cell in transfer_cells_params:
+                    session.delete(transfer_cell)
+                    session.commit()
+
+                _project = session.query(Projects).filter(Projects.id == id).first()
+                session.delete(_project)
+                session.commit()
+            _user_login = session.query(UserLogins).filter(UserLogins.user_id == user.id).first()
+            _user = session.query(Users).filter(Users.id == user.id).first()
+            session.delete(_user_login)
+            session.commit()
+            session.delete(_user)
+            session.commit()
+        analytic_rules = session.query(AnalyticRules).filter(AnalyticRules.client_id==client_id).all()
+        for rule in analytic_rules:
+            session.delete(rule)
+            session.commit()
         session.delete(client)
 
-
         session.commit()
-        return {}, 204
+        return {}, 200
 
     @marshal_with(client_fields)
     def put(self, id):
--- res/users_resources.py
+++ res/users_resources.py
@@ -1,4 +1,5 @@
-from db_models.models import Users, UserLogins
+from db_models.models import AnalyticRules, Clients, Users, UserLogins, Projects, Documents, ProjectControlLog, \
+    ProjectAnalysisLog, Reports, ReportForms, ProjectAnalysis, TransferCellsParams
 from db.db import session
 from flask import Flask, jsonify, request
 from flask_restful import Resource, fields, marshal_with, abort, reqparse
@@ -54,12 +55,63 @@ class UserResource(Resource):
         return user
 
     def delete(self, id):
-        client = session.query(Users).filter(Users.id == id).first()
-        if not client:
+        user = session.query(Users).filter(Users.id == id).first()
+        if not user:
             abort(404, message="User {} doesn't exist".format(id))
-        session.delete(client)
+        projects = session.query(Projects).filter(Projects.user_id == user.id).all()
+        for project in projects:
+            docs = session.query(Documents).filter(Documents.project_id == project.id).all()
+            reports = session.query(Reports).filter(Reports.project_id == project.id).all()
+            reportForms = session.query(ReportForms).filter(ReportForms.project_id == project.id).all()
+            logs = session.query(ProjectAnalysisLog).filter(ProjectAnalysisLog.project_id == project.id).all()
+            analysis = session.query(ProjectAnalysis).filter(ProjectAnalysis.project_id == project.id).all()
+            control_logs = session.query(ProjectControlLog).filter(ProjectControlLog.project_id == project.id).all()
+            transfer_cells_params = session.query(TransferCellsParams).filter(
+                TransferCellsParams.project_id == project.id).all()
+
+            for doc in docs:
+                session.delete(doc)
+                session.commit()
+
+            for report in reports:
+                session.delete(report)
+                session.commit()
+
+            for report_form in reportForms:
+                session.delete(report_form)
+                session.commit()
+
+            for log in logs:
+                session.delete(log)
+                session.commit()
+
+            for analys in analysis:
+                session.delete(analys)
+                session.commit()
+
+            for log in control_logs:
+                session.delete(log)
+                session.commit()
+
+            for transfer_cell in transfer_cells_params:
+                session.delete(transfer_cell)
+                session.commit()
+
+            _project = session.query(Projects).filter(Projects.id == id).first()
+            session.delete(_project)
+            session.commit()
+        _user_login = session.query(UserLogins).filter(UserLogins.user_id == user.id).first()
+        # _user = session.query(Users).filter(Users.id == user.id).first()
+        if (_user_login!=None):
+            session.delete(_user_login)
+            session.commit()
+        session.delete(user)
         session.commit()
-        return {}, 204
+
+
+        #session.delete(user)
+        #session.commit()
+        return {}, 200
 
     @marshal_with(user_fields)
     def put(self, id):
@@ -68,7 +120,7 @@ class UserResource(Resource):
         user = session.query(Users).filter(Users.id == id).first()
         user.first_name = json_data['first_name']
         user.last_name = json_data['last_name']
-        user.registration_number = json_data["registration_number"]
+        #user.registration_number = json_data["registration_number"]
         user.lock_state = json_data["lock_state"]
         user.client_id = json_data["client_id"]
         user.user_role_id = json_data["user_role_id"]
@@ -76,8 +128,10 @@ class UserResource(Resource):
         session.add(user)
 
         user_login = session.query(UserLogins).filter(UserLogins.user_id==id).first()
-
-
+        user_login.login =json_data["login_data"]["login"]
+        password = json_data["login_data"]["password"]
+        user_login.password = str(base64.b64encode(password.encode(encoding='utf-8')))
+        session.add(user_login)
 
         session.commit()
         return user, 201
@@ -97,7 +151,7 @@ class UserListResource(Resource):
                          lock_state=json_data["lock_state"], client_id=json_data["client_id"],
                          user_role_id=json_data["user_role_id"])
             session.add(user)
-            l = json_data["login_data"][0]
+            l = json_data["login_data"]
             session.commit()
             user_id = user.id
             log =l["login"]
