fa37443da4eeac85b0639460693326c681e8a88f
vyadzmak <vyadzmak@gmail.com>
2018-03-16 18:18:19 +0300

    fixed show year details

--- modules/details_converter.py
+++ modules/details_converter.py
@@ -9,37 +9,38 @@ import zlib
 import base64
 import copy
 
-from db_models.models import Users,Clients,AnalyticRules,Projects
+from db_models.models import Users, Clients, AnalyticRules, Projects
 from db.db import session
 from flask import Flask, jsonify, request
 from flask_restful import Resource, fields, marshal_with, abort, reqparse
 import json
 import result_models.res_model as r_m
 from sqlalchemy import and_
-
+import datetime
 def to_bytes(bytes_or_str):
     if isinstance(bytes_or_str, str):
-        value = bytes_or_str.encode() # uses 'utf-8' for encoding
+        value = bytes_or_str.encode()  # uses 'utf-8' for encoding
     else:
         value = bytes_or_str
-    return value # Instance of bytes
+    return value  # Instance of bytes
 
 
 def to_str(bytes_or_str):
     if isinstance(bytes_or_str, bytes):
-        value = bytes_or_str.decode() # uses 'utf-8' for encoding
+        value = bytes_or_str.decode()  # uses 'utf-8' for encoding
     else:
         value = bytes_or_str
-    return value # Instance of str
+    return value  # Instance of str
+
 
 def check_if_formula(type_id):
     try:
         f_letter = str(type_id)[0]
-        if (f_letter=='2'):
+        if (f_letter == '2'):
             n = int(type_id)
-            if (n>=250):
+            if (n >= 250):
                 return True
-            t=0
+            t = 0
 
         if (f_letter == '3'):
             n = int(type_id)
@@ -52,20 +53,20 @@ def check_if_formula(type_id):
         return False
         pass
 
-def get_formula_elements(project_id,type_id,analysis_type):
+
+def get_formula_elements(project_id, type_id, analysis_type):
     try:
         types = []
-        project = session.query(Projects).filter(Projects.id==int(project_id)).first()
-        if (project==None):
+        project = session.query(Projects).filter(Projects.id == int(project_id)).first()
+        if (project == None):
             return []
 
         user_id = project.user_id
 
-
         user_client = session.query(Users).filter(Users.id == user_id).first()
 
         if not user_client:
-                return []
+            return []
         client_id = user_client.client_id
 
         analytic_rules = session.query(AnalyticRules).filter(and_(
@@ -74,37 +75,38 @@ def get_formula_elements(project_id,type_id,analysis_type):
         ).first()
         model = json.loads(analytic_rules.data)
 
-        if (str(type_id).startswith('2') and str(analysis_type)=='1'):
-            #build to OSV
+        if (str(type_id).startswith('2') and str(analysis_type) == '1'):
+            # build to OSV
             pass
         else:
             if (str(type_id).startswith('2')):
                 formulas = model["opiu_rules"]["card_rules"]["cards_formulas"]["opiu_cards_formulas"]
                 for f in formulas:
-                    if (f["code"]==type_id):
+                    if (f["code"] == type_id):
                         for element in f["formula_elements"]:
-                           types.append(element['code'])
-                        et=0
-                y=0
+                            types.append(element['code'])
+                        et = 0
+                y = 0
                 pass
 
             if (str(type_id).startswith('3')):
                 formulas = model["odds_rules"]["odds_formulas"]["odds_formulas"]
                 for f in formulas:
-                    if (f["code"]==type_id):
+                    if (f["code"] == type_id):
                         for element in f["formula_elements"]:
-                           types.append(element['code'])
-                        et=0
-                y=0
+                            types.append(element['code'])
+                        et = 0
+                y = 0
                 pass
 
-        t =0
+        t = 0
         return types
         pass
     except Exception as e:
         return []
 
-def convert_details_by_period(documents,month,year,type_id,analysis_type,project_id):
+
+def convert_details_by_period(documents, month, year, type_id, analysis_type, project_id):
     try:
         result = ""
         headers = []
@@ -112,53 +114,64 @@ def convert_details_by_period(documents,month,year,type_id,analysis_type,project
         is_formula = check_if_formula(type_id)
         clear_table = []
         types = []
-        if (is_formula==False):
+        if (is_formula == False):
             types.append(type_id)
         else:
-            types = get_formula_elements(project_id,type_id,analysis_type)
-
+            types = get_formula_elements(project_id, type_id, analysis_type)
 
         for d in documents:
 
             s_cmpstr = copy.deepcopy(d.data)
             bc = s_cmpstr.count("b'")
 
-            s_cmpstr =s_cmpstr.replace("b'","",1)
+            s_cmpstr = s_cmpstr.replace("b'", "", 1)
             qc = s_cmpstr.count("'")
 
-            s_cmpstr =s_cmpstr.replace("'","")
-            b_cmpstr =to_bytes(s_cmpstr)
-            b_cmpstr =base64.b64decode(b_cmpstr)
+            s_cmpstr = s_cmpstr.replace("'", "")
+            b_cmpstr = to_bytes(s_cmpstr)
+            b_cmpstr = base64.b64decode(b_cmpstr)
             rr = to_str(zlib.decompress(b_cmpstr))
             f_cmpstr = rr
-            #f_cmpstr = f_cmpstr.replace("'", "")
+            # f_cmpstr = f_cmpstr.replace("'", "")
             rr = json.loads(f_cmpstr)
             try:
                 itms = rr["rows"][0]["cells"][0]["tableData"]["items"]
                 if (len(headers) == 0):
                     headers = rr["rows"][0]["cells"][0]["tableData"]["headers"]
-                tb=[]
+                tb = []
                 for tp in types:
 
-                    tb = [t for t in itms if
-                          (str(t["month"]) == str(month) and str(t["year"]) == str(year) and str(t["typeId"]) == str(tp))]
+                    tb = []
+                    if (year != 999):
+                        tb = [t for t in itms if
+                              (
+                              str(t["month"]) == str(month) and str(t["year"]) == str(year) and str(t["typeId"]) == str(
+                                  tp))]
+                        if (len(tb) > 0):
+                            result.append(tb)
+                    else:
+                        tb = [t for t in itms if
+                              (
+                                  str(t["typeId"]) == str(
+                                      tp))]
 
+                        if (len(tb) > 0):
+                            result.append(tb)
 
-                    if (len(tb) > 0):
-                        result.append(tb)
-
-            #genearate form
+            # genearate form
 
             except Exception as e:
                 t = 0
-
 
         for r in result:
             for t in r:
                 clear_table.append(t)
 
+        for p in clear_table:
+            p["period"] =  datetime.datetime.strptime(p["period"], '%d.%m.%Y').date()
+        clear_table.sort(key=lambda x: x["period"], reverse=False)
         form = a_f_m.AForm()
-        if (str(type_id).startswith('2') and str(analysis_type)=='1'):
+        if (str(type_id).startswith('2') and str(analysis_type) == '1'):
             #################################################
             form.add_row("Графики")
             row = form.get_last_row()
@@ -173,17 +186,17 @@ def convert_details_by_period(documents,month,year,type_id,analysis_type,project
                 values = []
 
                 for detail in clear_table:
-                    f_val = detail[c[0]]#(getattr(detail, c[0]))
-                    res = Decimal(sub(r'[^\d.]', '',  detail[c[1]]))
-                    s_val = float(res)#(getattr(detail, c[1]))
+                    f_val = detail[c[0]]  # (getattr(detail, c[0]))
+                    res = Decimal(sub(r'[^\d.]', '', detail[c[1]]))
+                    s_val = float(res)  # (getattr(detail, c[1]))
 
                     v = [f_val, s_val]
                     values.append(v)
-                all_nulls =True
+                all_nulls = True
 
                 for v in values:
-                    if (v[1]!=0):
-                        all_nulls =False
+                    if (v[1] != 0):
+                        all_nulls = False
                         break
                 if (all_nulls):
                     continue
@@ -194,7 +207,7 @@ def convert_details_by_period(documents,month,year,type_id,analysis_type,project
 
 
                 ##############################
-            #################################################
+                #################################################
         form.add_row("Данные")
         table = t_d_m.TableData()
         table.headers = headers
